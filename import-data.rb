718d396cbeca8df1ebc5f5a6bb02cb9757f72a61
Eric <ericrlindberg@gmail.com>
2019-10-06 10:43:22 -0700

    import csv can now be passed in as an argument when executing the script

--- import-data.rb
+++ import-data.rb
@@ -6,7 +6,7 @@ begin
   db = SQLite3::Database.open 'transactions.db'
   db.execute('CREATE TABLE IF NOT EXISTS transactions(id INTEGER PRIMARY KEY, date TEXT, amount REAL, account TEXT, category TEXT, notes TEXT);')
 
-  CSV.foreach('./upload-files/upload-with-headers.csv', converters: :numeric, headers: true) do |row|
+  CSV.foreach(ARGF.argv[0], converters: :numeric, headers: true) do |row|
     sql = "INSERT INTO transactions(date, amount, account, category, notes) VALUES(#{row['date'].inspect}, #{row['amount'].inspect}, #{row['account'].inspect}, #{row['category'].inspect}, #{row['notes'].inspect});"
     db.execute(sql)
   end
