61cd037165347409ee6ba623df08748f3420c25b
Eric Lindberg <ericrlindberg@gmail.com>
2019-12-14 14:03:29 -0800

    Normalize db (#3)
    * normalized the db, still need to modify the import script to normalize the unnormalized data in the import csv files
    
    * normalized the db and changed the import script to normalize the data in the csv import file

--- import-data.rb
+++ import-data.rb
@@ -1,13 +1,55 @@
 require 'csv'
 require 'sqlite3'
 
+bank_accounts = {
+  'WF Checking' => 1,
+  'WF Savings' => 2,
+  'DCCU Checking' => 3,
+  'DCCU Savings' => 4,
+  'DCCU $ Market' => 5
+}
+
+expense_categories = {
+  'AAB' => 1,
+  'Amazon Prime' => 2,
+  'Amex' => 3,
+  'Bluehost' => 4,
+  'Car Insurance' => 5,
+  'Car Maintenance' => 6,
+  'Cell Phone Service' => 7,
+  'Charter' => 8,
+  'Costco' => 9,
+  'Double Edge' => 10,
+  'Fitness' => 11,
+  'Food' => 12,
+  'FreeCodeCamp' => 13,
+  'Gas' => 14,
+  'Godaddy' => 15,
+  'Healthcare' => 16,
+  'Income' => 17,
+  'Lastpass' => 18,
+  'Life Lock' => 19,
+  'Lynda' => 20,
+  'Misc' => 21,
+  'Movies' => 22,
+  'Netflix' => 23,
+  'NV Energy' => 24,
+  'Rent' => 25,
+  'Shopping' => 26,
+  'TMWA' => 27,
+  'Transfer' => 28,
+  'TurboTax' => 29,
+  'Uber' => 30,
+  'Vacation' => 31,
+  'Waste Management' => 32
+}
+
 begin
 
-  db = SQLite3::Database.open 'budget.db'
-  db.execute('CREATE TABLE IF NOT EXISTS transactions(id INTEGER PRIMARY KEY, date TEXT, amount REAL, account TEXT, category TEXT, notes TEXT);')
+  db = SQLite3::Database.open 'test.db'
 
   CSV.foreach(ARGF.argv[0], converters: :numeric, headers: true) do |row|
-    sql = "INSERT INTO transactions(date, amount, account, category, notes) VALUES(#{row['date'].inspect}, #{row['amount'].inspect}, #{row['account'].inspect}, #{row['category'].inspect}, #{row['notes'].inspect});"
+    sql = "INSERT INTO Transactions(transaction_date, transaction_amount, bank_account_id, expense_category_id, notes) VALUES(#{row['transaction_date'].inspect}, #{row['transaction_amount']}, #{bank_accounts[row['bank_account_id']]}, #{expense_categories[row['expense_category_id']]}, #{row['notes'].inspect});"
     db.execute(sql)
   end
 
